fa87441e9bc31af819e97c3c8db2c8354b552ac7
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-12-10 21:44:57 +0530

    added file upload

--- backend/gateway_service/app/core/file_core.py
+++ backend/gateway_service/app/core/file_core.py
@@ -1,4 +1,37 @@
-async def file_upload():
+import json
+import pika
+
+
+async def file_upload(file_descriptor, gridfs, channel, user_id):
     """
     This method will be responsible for the file upload functionality.
+    """
+    try:
+        fid = gridfs.put(file_descriptor)
+    except Exception as err:
+        return err
+
+    message = {
+        "video_fid": str(file_descriptor),
+        "mp3_fid": None,
+        "user_id": user_id,
+    }
+
+    try:
+        channel.basic_publish(
+            exhange="",
+            routing_key="video",
+            body=json.dumps(message),
+            properties=pika.BasicProperties(
+                delivery_mode=pika.__spec__.PERSISTENT_DELIVERY_MODE
+            ),
+        )
+    except:
+        gridfs.delete(fid)
+        return "error"
+
+
+async def file_download():
+    """
+    This method will be responsible for the file download functionality.
     """
